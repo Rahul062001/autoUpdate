3a5eda55fadbf8233e09cccfd218a00b7a9da9c3
rahul.suthar <rahul.suthar@ethicsinfotech.in>
2023-10-23 12:26:23 +0530

    add updater code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { autoUpdater } = require('electron-updater');
+const log = require("electron-log");
+log.transports.file.resolvePath = () => path.join('D:\Electron\Electron-Auto-Update','logs/main.log');
+log.info("Electron Auto Update");
 
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -19,7 +23,7 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools();
 };
 
-app.on('ready', createWindow);
+// app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -27,8 +31,48 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+// app.on('activate', () => {
+//   if (BrowserWindow.getAllWindows().length === 0) {
+//     createWindow();
+//   }
+// });
+
+app.whenReady().then(() => {
+  createWindow();
+
+  autoUpdater.checkForUpdatesAndNotify();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = true;
+
+autoUpdater.on("update-available", (info)=>{
+  log.info("update-available",info);
+});
+
+autoUpdater.on("checking-for-update", (info)=>{
+  log.info("checking-for-update",info);
+});
+
+autoUpdater.on("update-downloaded", (info)=>{
+  log.info("update-downloaded", info);
+});
+
+autoUpdater.on("download-progress", (progressTrack)=>{
+  log.info("download-progress")
+  log.info(progressTrack);
+});
+
+autoUpdater.on("update-not-available", (info)=>{
+  log.info("update-not-available",info);
+});
+
+autoUpdater.on('error', (message) => {
+  log.info('There was a problem updating the application', message)
 });
